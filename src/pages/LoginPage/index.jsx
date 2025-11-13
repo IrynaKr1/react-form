7f8a8108084d1f1bfaec7329c32cf60229a2cf33
IrynaKr1 <krutsiryna@gmail.com>
2025-11-13 14:26:15 +0100

    Add loginPage styles

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import LoginForm from '../../components/forms/LoginForm';
 import Header from '../../components/Header';
+import styles from './LoginPage.module.scss';
 
 function LoginPage() {
   return (
-    <section>
-    <Header />
-      <h2>Login to your account</h2>
-      <LoginForm />
-    </section>
+    <div className={styles.loginPageContainer}>
+      <Header />
+      <div className={styles.contentWrapper}>
+        <h2 className={styles.title}>Login to your account</h2>
+        <LoginForm />
+      </div>
+    </div>
   );
 }
 
