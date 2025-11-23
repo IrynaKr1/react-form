fe7a3630a93247b050d3a3d139b43be74f4b0861
IrynaKr1 <krutsiryna@gmail.com>
2025-11-23 15:08:01 +0100

    Add better password validation regex

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,4 +16,4 @@ export const PASSWORD_VALIDATION_SCHEMA = yup
   .string()
   .required('No password provided')
   .min(8, 'Password is too short - should be 8 chars minimum.')
-  .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.');
+  .matches(/^[a-zA-Z0-9]+$/, 'Password can only contain Latin letters and numbers.');
